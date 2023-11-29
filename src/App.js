37538fb069638c2f3c050b3505b2816507ed4d13
roy maman <roey.maman@gmail.com>
2023-11-29 19:46:04 +0200

    a

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import SmileyPage from './pages/SmileyPage';
 import io from 'socket.io-client';
 
 const App = () => {
-  const baseurl = 'https://moveoprojectserver-production.up.railway.app/'
   const [sessionNumber, setSessionNumber] = useState(null); // Use null as the initial state
   const [isLoading, setIsLoading] = useState(true); // Loading state
 
   useEffect(() => {  
-    const socket = io(baseurl);
+    const socket = io('http://localhost:5000');
     socket.on('connect', () => {
       setSessionNumber(socket.id);
       setIsLoading(false); // Set loading to false when session number is available
@@ -24,15 +23,15 @@ const App = () => {
   return (
     <div className="app-container">
       <Router>
-        {/* {isLoading ? ( // Render loading state until sessionNumber is available
+        {isLoading ? ( // Render loading state until sessionNumber is available
           <p>Loading...</p>
-        ) : ( */}
+        ) : (
           <Routes>
             <Route path="/" element={<LobbyPage sessionNumber={sessionNumber} />} />
             <Route path="/Coding/:title" element={<CodingInput sessionNumber={sessionNumber} />} />
             <Route path="/Smiley" element={<SmileyPage/>} />
           </Routes>
-        {/* )} */}
+        )}
       </Router>
     </div>
   );
